f568218111bdfd2d6d786cbc4cb7783e2bf18efe
doisn2003 <ngocdoisamsung@gmail.com>
2025-12-21 05:30:29 +0700

    Checkout

--- fe/front_end/src/App.jsx
+++ fe/front_end/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Checkout from "./pages/Checkout/Checkout";
+
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
@@ -14,6 +16,7 @@ function App() {
       <Navbar setShowLogin={setShowLogin} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
       <Footer />
     </>

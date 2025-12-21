09b7f0aaffa885785833cde790a24b943b8f6803
DATMQHUST <Dat.mq225277@sis.hust.edu.vn>
2025-12-21 10:53:24 +0700

    test deploy v5

--- server/src/server.js
+++ server/src/server.js
@@ -11,5 +11,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server đang chạy trên cổng ${PORT}`);
 });
